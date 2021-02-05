00fc9f3946bed0d638e946e6683a44c85390da0e
cacastillomo03 <castillomoran@hotmail.com>
2021-02-05 15:49:26 -0500

    Nueva actualizacion a interfaces

--- app/src/main/java/com/example/myapplication/RegistroUsuarios.java
+++ app/src/main/java/com/example/myapplication/RegistroUsuarios.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Registrarusuarios extends AppCompatActivity {
+public class RegistroUsuarios extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrarusuarios);
+        setContentView(R.layout.activity_registro_usuarios);
     }
 }
