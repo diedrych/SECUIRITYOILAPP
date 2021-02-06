434477d9797509848a76b7ec09cdecd5aedc56fc
Solange Jiménez <dsja-angel@hotmail.com>
2021-02-06 15:28:03 -0500

    desarollo en newforms"

--- app/src/main/java/com/example/myapplication/newsForm.java
+++ app/src/main/java/com/example/myapplication/newsForm.java
@@ -9,6 +9,7 @@ import androidx.core.content.FileProvider;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -268,7 +269,7 @@ public class newsForm extends AppCompatActivity {
    //metodo para valida que los campos esten llenos
    private boolean validateInputs(String prior, String cond, String desc) {
        if (prior.isEmpty()) {
-           autoCompletePriority.setError("Ingrese la prioridad");
+          // autoCompletePriority.setError("Ingrese la prioridad");
            autoCompletePriority.requestFocus();
            return true;
        }
