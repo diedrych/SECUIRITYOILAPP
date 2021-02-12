f9901ec1b4646a3aa0cbd3ff2de97a2fb766c6d4
Solange Jiménez <dsja-angel@hotmail.com>
2021-02-12 18:34:18 -0500

    desarrollo de cambio de activity

--- app/src/main/java/com/example/myapplication/PruebaInicioSesion.java
+++ app/src/main/java/com/example/myapplication/PruebaInicioSesion.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PruebaInicioSesion extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class PruebaInicioSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba_inicio_sesion);
+    }
+
+    public void passNewFormppa(View view){
+        Intent i = new Intent(this, NewsReportMenu.class);
+        startActivity(i);
     }
 }
