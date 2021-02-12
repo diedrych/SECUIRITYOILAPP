5cea927dda8472cd43d9ab9be8aefcd4fc2e7d82
cesargalvis <78121818+cesargalvis@users.noreply.github.com>
2021-02-12 18:45:03 -0500

    direccionamiento elearning

--- app/src/main/java/com/example/myapplication/PruebaInicioSesion.java
+++ app/src/main/java/com/example/myapplication/PruebaInicioSesion.java
@@ -2,13 +2,24 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class PruebaInicioSesion extends AppCompatActivity {
 
+    ImageButton elearning;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba_inicio_sesion);
+    }
+
+    //e_learning boton
+    public void elearning(View view){
+        Intent i = new Intent(this, e_learning.class);
+        startActivity(i);
     }
 }
