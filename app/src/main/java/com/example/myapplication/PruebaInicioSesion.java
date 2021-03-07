033b58a0b39007d829beca56623950d1ef7723fa
jjgaviria2601 <jerlinton@hotmail.com>
2021-03-07 15:47:40 -0500

    Merge branch 'estadistica' of https://github.com/diedrych/SECUIRITYOILAPP into devTest
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/myapplication/MainActivity.java
    #	app/src/main/java/com/example/myapplication/NewsReportMenu.java
    #	app/src/main/java/com/example/myapplication/newsForm.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/myapplication/PruebaInicioSesion.java
+++ app/src/main/java/com/example/myapplication/PruebaInicioSesion.java
@@ -2,13 +2,23 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class PruebaInicioSesion extends AppCompatActivity {
+
+    ImageButton imageButton6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba_inicio_sesion);
+
+        imageButton6 = findViewById(R.id.imageButton6);
+
+        imageButton6.setOnClickListener(v ->{
+            Intent intent = new Intent(this, ReportsActivity.class);
+        });
     }
 }
